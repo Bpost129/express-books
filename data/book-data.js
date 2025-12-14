5c8d496a15c7f771ef6e480d387e4edee33b0860
Bpost129 <Bpost129@yahoo.com>
2025-12-13 22:08:15 -0500

    add book data, refactor code to reflect resourse change, update route to render books index view

---
+++ data/book-data.js
@@ -0,0 +1,9 @@
+const books = [
+  {title: 'The Cat In The Hat', done: true, _id: 125223},
+  {title: 'The Outsiders', done: false, _id: 127904},
+  {title: 'Atomic Habits', done: true, _id: 139608},
+]
+
+export { 
+	books
+}
--- routes/books.js
+++ routes/books.js
@@ -4,7 +4,9 @@ const router = Router()
 
 // GET localhost:3000/users
 router.get('/', function(req, res) {
-  res.send('respond with a resource')
+  res.render('books/index', {
+    books: books
+  })
 })
 
 export { router }
