1c364b305b1c1781dd0d3c9b7dcb582223681b7a
Bpost129 <Bpost129@yahoo.com>
2025-12-14 13:20:49 -0500

    add record to book data

--- data/book-data.js
+++ data/book-data.js
@@ -2,6 +2,7 @@ const books = [
   {title: 'The Cat In The Hat', done: true, _id: 125223},
   {title: 'The Outsiders', done: false, _id: 127904},
   {title: 'Atomic Habits', done: true, _id: 139608},
+  {title: 'War and Peace', done: false, _id: 148916},
 ]
 
 export { 
